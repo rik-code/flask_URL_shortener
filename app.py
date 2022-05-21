ecf1acbf1e2ed9656ba13cd55cbb24b511dcab33
Alexey Rytsku <topelven1234567890@gmail.com>
2022-05-21 15:38:46 +0300

    Fixing bug

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import sqlite3
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row  # получает строки из БД
     return conn
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'try-to-guess'
@@ -17,7 +15,6 @@ app.config['SECRET_KEY'] = 'try-to-guess'
 def index():
     conn = get_db_connection()  # подключаемся к БД
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run()
